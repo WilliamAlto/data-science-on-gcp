9e0ad498b153c1b4007ad438c4983783d4cdb1b3
William Alto <45225540+WilliamAlto@users.noreply.github.com>
2019-05-10 10:27:17 -0700

    Update upload.sh

--- 02_ingest/upload.sh
+++ 02_ingest/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export BUCKET=${BUCKET:=data-science-on-the-gcp-239619}
 echo "Uploading to bucket $BUCKET..."
-gsutil -m cp *.csv gs://$BUCKET
+gsutil -m cp *.csv gs://$BUCKET/data
 #gsutil -m acl ch -R -g google.com:R gs://$BUCKET/flights/raw
